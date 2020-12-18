16efa42b250ff234546496b7739feb0488edd6d0
Fidellis-Taz <fidellis.dango@gmail.com>
2020-12-18 23:15:58 +0200

    sb connected

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,38 @@
 const express = require("express");
-
 const app = express();
+const bodyParser = require("body-parser");
+const env = require("dotenv");
+const mongoose = require("mongoose");
+/* environment variable */
+env.config();
+
+/* middleware */
+app.use(bodyParser()); //more like express.json middleware
+
+/* db connection */
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.cz7rv.mongodb.net/${process.env.MONGO_DB_DATABSE}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("db connected");
+  });
+
+app.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "helloee from server ",
+  });
+});
+app.get("/data", (req, res, next) => {
+  res.status(200).json({
+    message: res.body,
+  });
+});
 
 app.listen(process.env.PORT, () => {
-  console.log("server is running");
+  console.log("server is running ");
 });
